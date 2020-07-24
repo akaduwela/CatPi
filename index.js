062f800d2131c106f25f492d37887e30c296d698
akaduwela <asithank@gmail.com>
2020-07-23 22:45:26 -0700

    Home Page Working

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert('hello')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => console.log('CatPi app listening on port 3000'));
